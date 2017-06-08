fb6ad4e200cbd306d77c28ccd5b273180c511b2c
andy1729 <ujjawalanand1729@gmail.com>
2017-06-08 11:02:42 +0530

    created blank activity and fragment

---
+++ app/src/main/java/io/uscool/quizapp/activities/ShowSubjectActivity.java
@@ -0,0 +1,16 @@
+package io.uscool.quizapp.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import io.uscool.quizapp.R;
+
+public class ShowSubjectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_subject);
+    }
+
+}
